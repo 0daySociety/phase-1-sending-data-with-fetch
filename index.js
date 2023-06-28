522483f36f1b7ddd6cc67b8816791f76c267c848
--replace <david.thuku@studentmoringa.school.com>
2023-06-28 17:33:06 +0300

    i am getting the following error   Uncaught TypeError [ERR_INVALID_ARG_TYPE]: The stream argument must be an instance of ReadableStream, WritableStream, or Stream. Received an instance of Socket in the learn-lab test command

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+
+
+function submitData(usersname, myemail){
+let url;
+
+if (usersname !=="" && myemail !==""){   
+    
+    url = "http://localhost:3000/users"}
+
+    else{
+        document.body.innerHTML=`<p> no data </p>` ;  
+        return; 
+    } 
+
+    console.log("data ")
+ fetch(url,{
+    method:"POST",
+    headers:{"Content-Type":"application/json",
+             "Accept":"application/json"},
+
+    body:JSON.stringify({name:usersname,email:myemail})
+
+}).then(response=>response.json())
+  .then(data=>document.body.innerHTML=`<p>You were added with the id  ${data.id}</p>`)
+  .catch(error=>document.body.innerHTML=error)
+}
+
+
+submitData("david","username")
+
+
+
+
